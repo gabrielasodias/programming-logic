dfd258ad9a1b72db1f621d782d238e299c3a0bb0
Gabriela Dias <gabrieladiasofc@gmail.com>
2024-02-08 15:30:29 -0300

    Equação do segundo grau

---
+++ python/segundograu.py
@@ -0,0 +1,18 @@
+from math import sqrt
+
+print('\nEquação do Segundo Grau\n')
+
+a = int (input('Digite o valor de A: '))
+b = int (input('Digite o valor de B: '))
+c = int (input('Digite o valor de C: '))
+
+delta = b**2 - 4*a*c
+ 
+if delta < 0:
+    print("Delta negativo")
+else:
+    raiz_delta = sqrt(delta)
+    x1 = (-b + raiz_delta)/2*a
+    x2 = (-b - raiz_delta)/2*a
+ 
+    print(f'As raízes são {x1} e {x2}')
