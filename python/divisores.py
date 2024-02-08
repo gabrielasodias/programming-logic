8834106c6141c6aebb457886505f7110680fdf6d
Gabriela Dias <gabrieladiasofc@gmail.com>
2024-02-08 15:12:55 -0300

    Divisores de um número

---
+++ python/divisores.py
@@ -0,0 +1,14 @@
+print('\nDivisores de um número')
+
+numero = int(input('Digite um número inteiro: ')) #Entrada de um número inteiro
+
+lista_range = list(range(1,numero+1)) #Cria uma lista com uma sequência baseada no número digitado
+
+divisor_list = [] #Lista para os divisores
+
+for i in lista_range: #Passa por todos os elementos da lista
+  if numero%i == 0: #Verifica se é um divisor
+    divisor_list.append(i) #Se for divisor, acrescenta na lista divisor_list
+
+print('\nOs divisores são: ')
+print(divisor_list) #Exibe a lista de divisores
