301b675f96d2995077103d3282ca4c0fca5ba8da
Gabriela Dias <gabrieladiasofc@gmail.com>
2024-02-10 14:21:24 -0300

    Comentários no código de cálculo da função do segundo grau

--- python/funcao-do-segundo-grau/segundograu.py
+++ python/funcao-do-segundo-grau/segundograu.py
@@ -1,18 +1,19 @@
-from math import sqrt
+from math import sqrt #Biblioteca para uso da função
 
 print('\nEquação do Segundo Grau\n')
 
+#Entrada dos valores
 a = int (input('Digite o valor de A: '))
 b = int (input('Digite o valor de B: '))
 c = int (input('Digite o valor de C: '))
 
-delta = b**2 - 4*a*c
- 
-if delta < 0:
+delta = b**2 - 4*a*c #Calcula o delta
+
+if delta < 0: #Para delta negativo
     print("Delta negativo")
-else:
-    raiz_delta = sqrt(delta)
+else: #Calcula as raízes
+    raiz_delta = sqrt(delta) 
     x1 = (-b + raiz_delta)/2*a
     x2 = (-b - raiz_delta)/2*a
  
-    print(f'As raízes são {x1} e {x2}')
+    print(f'As raízes são {x1} e {x2}') #Exibi o resultado
